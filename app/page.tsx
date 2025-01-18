3bf22a6e6fa6a583637ade7ba09ffa26c0c85ab8
Collins Aniesodo <colinsanisedo@gmail.com>
2025-01-18 18:10:40 +0100

    eight commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,46 +8,29 @@ import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 export default function Home() {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+  <div className="flex flex-1 justify-end h-full items-end relative">
+    <CardItem translateZ="100" className="pl-3 z-10 relative absolute bottom-1/2 translate-y-1/2 right-20 w-60 h-60">
+        <Image
+          src="/image.png"
+          height="1000"
+          width="1000"
+          className="rounded-xl group-hover/card:shadow-xl"
+          alt="thumbnail"
+        />
+    </CardItem>
+    <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  rounded-xl p-6 border ">
+    <div className="flex justify-between items-center mt-20">
         <CardItem
-          translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          translateZ={20}
+          target="__blank"
+          className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
         >
-          Make things float in air
+          Try now →
         </CardItem>
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-        >
-          Hover over this card to unleash the power of CSS perspective
-        </CardItem>
-        <CardItem translateZ="100" className="w-full mt-4">
-          <Image
-            src="/image.png"
-            height="1000"
-            width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
-        </CardItem>
-        <div className="flex justify-between items-center mt-20">
-          <CardItem
-            translateZ={20}
-            target="__blank"
-            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-          >
-            Try now →
-          </CardItem>
-          <CardItem
-            translateZ={20}
-            as="button"
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-          >
-            Sign up
-          </CardItem>
-        </div>
-      </CardBody>
-    </CardContainer>
-  );
+        
+      </div>
+    </CardBody>
+  </div>
+</CardContainer>
+  )
 }
